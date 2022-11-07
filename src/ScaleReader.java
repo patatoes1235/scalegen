5e9afd5b3c647dac51778b4e7c1edc16fbbee3f3
patatoes1235 <kzhou12356@gmail.com>
2022-11-07 11:31:15 -0600

    initial commit

--- src/ScaleReader.java
+++ src/ScaleReader.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+
 public class ScaleReader {
     private ArrayList<String> scales;
     public ScaleReader(String fileName) {
         File scaleFile = new File(fileName);
-        Scanner input = new Scanner(fileName);
+        Scanner input = new Scanner(scaleFile);
+        while ()
     }
 }
